557c8c4becc9933a9a5802406faadfb054f65f57
Drunkard Zhang <gongfan193@gmail.com>
2017-02-21 17:06:18 +0800

    tool: add tool to open two versions of same file in vim
    Signed-off-by: Drunkard Zhang <gongfan193@gmail.com>

---
+++ vi_two_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CN_PATH=$(dirname `realpath $0`)
+EN_PATH="/git/ceph/doc-en"
+
+open_files() {
+	cn_doc="$CN_PATH/$1"
+	en_doc="$EN_PATH/$1"
+
+	[ -e $cn_doc ] || {
+		echo "中文版文档不存在: $1"
+		return 1
+	}
+	[ -e $en_doc ] || {
+		echo "英文版文档不存在: $1"
+		return 1
+	}
+
+	vim -O $cn_doc $en_doc
+}
+
+if [ $# -ge 1 ]; then
+	for f in $@; do
+		open_files "$f"
+	done
+else
+	cat <<-EOF
+	指定一个文件名即可同时打开文档的中文版和英文，指定多个文件则依次打开
+	EOF
+fi
