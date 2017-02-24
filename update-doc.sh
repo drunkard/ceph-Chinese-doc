9ea0bc44e4b1f95007120fe4104b670792dc0ac3
Drunkard Zhang <gongfan193@gmail.com>
2017-02-24 11:33:18 +0800

    tool: update by date, not by commit
    If update by commit, and we did't finish the branch and we may
    lose some commits of that branch, by date is more reliable.
    
    Signed-off-by: Drunkard Zhang <gongfan193@gmail.com>

--- update-doc.sh
+++ update-doc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Where we synced with ceph mainline:
-CUR=d5cee59b024f9acc594d727c1a776efc051f3f09
+# Where we synced with ceph mainline. Note by date is more reliable than
+# commit ID, because there's too many criss-cross branches which is hard
+# for us to sync by branch/commit ID.
+SYNC_START=2015-12-23
 
 MYPATH="${0%/*}"
 CEPH_REPO=/git/ceph
 if cd $CEPH_REPO; then
-	echo "There's `git log --oneline ${CUR}.. doc/ | wc -l` commits to sync"
-	gitview --reverse --date-order ${CUR}.. -- doc/ &
+	echo "There's `git log --oneline --since=${SYNC_START} doc/ | wc -l` commits to sync"
+	gitview --reverse --date-order --since=${SYNC_START} -- doc/ &
 fi
