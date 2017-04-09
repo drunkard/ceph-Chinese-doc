4f4c1694d1c82a1c96e88106e02a28b90ca84270
Drunkard Zhang <gongfan193@gmail.com>
2017-04-09 22:12:08 +0800

    doc: sync with mainline
    Updated to: "2016-06-01"
    
    Signed-off-by: Drunkard Zhang <gongfan193@gmail.com>

--- update-doc.sh
+++ update-doc.sh
@@ -2,7 +2,7 @@
 # Where we synced with ceph mainline. Note by date is more reliable than
 # commit ID, because there's too many criss-cross branches which is hard
 # for us to sync by branch/commit ID.
-SYNC_START="2016-05-24"
+SYNC_START="2016-06-01"
 
 if ! /usr/bin/which tig &>/dev/null; then
 	echo "Need command line tool: tig"
