a1b843cd8c2d4f4d9793b1428664d43c721536fa
Drunkard Zhang <gongfan193@gmail.com>
2020-07-14 19:38:08 +0800

    doc: sync with mainline, updated to: 2019-01-02
    Signed-off-by: Drunkard Zhang <gongfan193@gmail.com>

--- translation_cn/env.sh
+++ translation_cn/env.sh
@@ -42,7 +42,7 @@ fi
 # Where we synced with ceph mainline. Note by date is more reliable than
 # commit ID, because there's too many criss-cross branches which is hard
 # for us to sync by branch/commit ID.
-SYNC_TO="2018-12-23"
+SYNC_TO="2019-01-02"
 PROGRESS_FILE="translation_cn/env.sh"
 
 # sync by STEP days once, 0 to disable
