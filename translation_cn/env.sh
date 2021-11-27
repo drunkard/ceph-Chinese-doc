ec6167c11ccaa96e1344d3646e5c7eb3e156b0ef
Drunkard Zhang <gongfan193@gmail.com>
2021-11-27 09:30:43 +0800

    doc: sync with mainline, updated to: 2021-06-26
    Signed-off-by: Drunkard Zhang <gongfan193@gmail.com>

--- translation_cn/env.sh
+++ translation_cn/env.sh
@@ -46,7 +46,7 @@ ZH_YAML="$ZH_REPO/zh_options"
 # commit ID, because there's too many criss-cross branches which is hard
 # for us to sync by branch/commit ID.
 # sync by $STEP days once, 0 to disable
-SYNC_TO="2021-06-19"
+SYNC_TO="2021-06-26"
 STEP=7
 SYNC_UNTIL=`date -d "$SYNC_TO +$STEP days" +%Y-%m-%d`
 
