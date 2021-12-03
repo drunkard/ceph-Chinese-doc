5a42032e24f0d4259ea668459dc814b6b5301322
Drunkard Zhang <gongfan193@gmail.com>
2021-12-03 21:29:07 +0800

    tool: new feature to count translation progress
    Signed-off-by: Drunkard Zhang <gongfan193@gmail.com>

--- translation_cn/improve_needs.py
+++ translation_cn/improve_needs.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import os
+import pandas as pd
 import path
+import re
 
 
 doc_cn = '/git/DRUNKARD/ceph-Chinese-doc'
@@ -22,6 +24,27 @@ IGNORE_FILES = [
     'translation_cn',
     'zh_options',
 ]
+
+# 翻译进度，需要统计的子系统列表
+SUBSYS = [
+    # 'api',
+    'cephadm',
+    'cephfs',
+    'ceph-volume',
+    # 'dev',
+    'install',
+    'jaegertracing',
+    'man',
+    'mgr',
+    # 'mon',
+    'rados',
+    'radosgw',
+    'rbd',
+    'security',
+    'start',
+]
+# Single file to debug, will be ignored if it's empty
+FILEP = ''
 
 
 def compare_file_existency():
@@ -61,6 +84,17 @@ def compare_file_length():
     print()
 
 
+def count_file_progress(f):
+    ''' 单个文件的翻译进度 '''
+    cn, total = 0, 0
+    with open(f) as fo:
+        for line in fo.readlines():
+            total += 1
+            if is_translated(line):
+                cn += 1
+    return (cn, total)
+
+
 def _file_row_counts(*file_names):
     return len(open(os.path.join(*file_names)).readlines())
 
@@ -95,11 +129,68 @@ def _get_file_list(directory, only_rst=False, relpath=False):
     return sorted(efl)
 
 
+def is_translated(line):
+    '''
+    比较规则：
+    按行比较，有汉字就视作翻译了；
+    以 [a-zA-Z] 打头的才计算；
+    空行、[.-`*#:\] 、\s\t 打头的不算；
+    '''
+    cn_char = re.compile(r'[\u4e00-\u9fa5“”（）…—！《》，。：]')  # 匹配汉字
+    starts = re.compile(r'^[\.\-=~_\^+|`*#:\(\)\[\]\\\s\t\"\'0-9]')  # 匹配行首
+    if not line:  # 空行
+        return True
+    if cn_char.search(line) or starts.match(line):
+        return True
+    if FILEP:
+        print(line, end='')  # debug, to catch exceptions of re expr
+    return False
+
+
+def to_pct(a, b):
+    return '{}%'.format(round(a / b * 100, 2))
+
+
 def translate_progress():
-    # 按行比较，全英文的视为没翻译
-    pass  # TODO
+    # 统计结果暂存
+    progress = pd.DataFrame(columns=['subsys', 'file', 'translated', 'total', 'pct'])
+    # TODO: left align 'file' when show_all
+    # pd.style.set_properties(**{'text-align': 'left'})\
+    #     .set_table_styles([ dict(selector='th', props=[('text-align', 'left')]) ])
+    idx = 0
+    print('Progress by subsys:')
+    for subsys in SUBSYS:
+        print('{:<36}'.format(f'    counting subsys {subsys}/'), end='')
+        files = _get_file_list(subsys, only_rst=True)
+        for f in files:
+            trans, total = count_file_progress(f)
+            progress.loc[idx] = [subsys, f, trans, total, trans / total]
+            idx += 1
+        subsys_prog = progress[progress.subsys == subsys]
+        r = subsys_prog.agg({'translated': sum, 'total': sum})
+        print(to_pct(r.translated, r.total))
+
+    # 总进度
+    tp = progress.agg({'translated': sum, 'total': sum})
+    print('Overall progress: \t\t{}'.format(to_pct(tp.translated, tp.total)))
+
+    print('\nProgress by file: \n',
+          progress.where(progress.pct != 1)\
+          .sort_values('pct', ascending=False)\
+          .dropna()\
+          .head(50))
 
 
 if __name__ == "__main__":
     compare_file_existency()
     compare_file_length()
+
+    if FILEP:
+        print(FILEP, count_file_progress(FILEP))
+    else:
+        # DataFrame 显示所有数据
+        # pd.set_option('display.max_rows', 100)
+        # pd.set_option('display.max_columns', None)
+        # pd.set_option('display.width', None)
+        # pd.set_option('display.max_colwidth', None)
+        translate_progress()
